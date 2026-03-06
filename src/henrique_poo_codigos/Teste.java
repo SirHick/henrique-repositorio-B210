8bf575d3c33c150eea94852c50f37a4d67519c1e
SirHick <henriquedahmer1234@gmail.com>
2026-03-06 08:01:13 -0300

    código teste
    Coloquei um código de teste

---
+++ src/henrique_poo_codigos/Teste.java
@@ -0,0 +1,11 @@
+package henrique_poo_codigos;
+
+public class Teste {
+	
+	String nome;
+	
+	void mostrarNome(){
+		System.out.println(nome);
+	}
+	
+}
---
+++ src/henrique_poo_codigos/TesteMain.java
@@ -0,0 +1,14 @@
+package henrique_poo_codigos;
+
+public class TesteMain {
+
+	public static void main(String[] args) {
+		
+		Teste teste1 = new Teste();
+		
+		teste1.nome = "Diego";
+		
+		teste1.mostrarNome();
+	}
+
+}
