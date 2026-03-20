8af673ec22e35a06147f722c5f92faa492a7d0d2
SirHick <henriquedahmer1234@gmail.com>
2026-03-20 08:37:54 -0300

    Update Pix.java

--- src/exemplosPolimorfismo/Pix.java
+++ src/exemplosPolimorfismo/Pix.java
@@ -17,6 +17,23 @@ public class Pix extends Pagamento{
 		this.chavePix = chavePix;
 	}
 	
+	@Override
+	// Sobrescrita do método com polimorfismo
+	public void processarPagamento() {
+		System.out.println("PROCESSANDO PIX\n");
+		System.out.println("Titular: " + getTitular());
+		System.out.println("Valor: R$ " + String.format("%.2f", getValor()));
+		System.out.println("Chave PIX: " + chavePix);
+		
+		System.out.println("Taxa: R$ " + String.format("%.2f", calcularTaxa()));
+		System.out.println("Total: R$ " + String.format("%,2f", getValor() + calcularTaxa()));
+		System.out.println("Transferência PIX realizada!");
+	}
 	
+	@Override
+	
+	public double calcularTaxa() {
+		return 0.0; //PIX não tem taxa
+	}
 	
 }
