3b8e1807fb673aa42189cf2c90866801eb073964
SirHick <henriquedahmer1234@gmail.com>
2026-04-06 07:50:44 -0300

    Add Pessoa model, DAO, main and DB script

---
+++ lib/henrique_bd.sql
@@ -0,0 +1,10 @@
+create database db_java;
+use db_java;
+
+create table pessoas(
+	id int auto_increment primary key,
+    nome varchar(100) not null,
+    idade int not null
+);
+
+select * from pessoas;
