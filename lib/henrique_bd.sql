300b3c82632f9cda08b10b2845e497f7646a3005
Henrique Miguel Deschamps Dahmer <henriquedahmer1234@gmail.com>
2026-04-23 12:32:53 -0300

    comecei o segundo exercício

--- lib/henrique_bd.sql
+++ lib/henrique_bd.sql
@@ -1,10 +1,11 @@
 create database db_java;
 use db_java;
 
-create table pessoas(
+create table pessoa(
 	id int auto_increment primary key,
     nome varchar(100) not null,
     idade int not null
+    cpf varchar(20) not null
 );
 
-select * from pessoas;
+select * from pessoa;
